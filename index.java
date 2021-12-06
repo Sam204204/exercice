52edcdc44f039978730c5d165828f9c81aedf203
Samuel <samuelpasqualotto204@gmail.com>
2021-12-06 18:04:48 +0100

    Version 2

--- index.java
+++ index.java
@@ -5,7 +5,10 @@ public static void main(String[] args) {
     int a = 10;
     int b = 5;
     int c = a+b;
+    int d = 20;
+    int e = d-c;
     System.out.println("10+5= "+c);
+    System.out.println("20-15 =" +e);
 
 }
 }
