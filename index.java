4018df33290b031d5c9aa1b9498b63c1f9fc2f6c
Samuel <samuelpasqualotto204@gmail.com>
2021-12-06 18:00:57 +0100

    Première version

---
+++ index.java
@@ -0,0 +1,14 @@
+import javax.sound.sampled.SourceDataLine;
+
+public  class test  {
+public static void main(String[] args) {
+    int a = 10;
+    int b = 5;
+    int c = a+b;
+    System.out.println("10+5= "+c);
+
+}
+}
+
+
+    
